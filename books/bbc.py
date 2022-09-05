9ee6bb631bf74a61863863fba165233fbb69479d
nofoghu <62465691+nofoghu@users.noreply.github.com>
2022-09-05 19:32:36 +0800

    Add files via upload

---
+++ books/bbc.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from base import BaseFeedBook
+
+def getBook():
+    return BBCChinese
+
+class FTChinese(BaseFeedBook):
+    title                 = u'BBC中文网'
+    description           = u'英国广播公司中文网站。'
+    language              = 'zh-cn'
+    feed_encoding         = "utf-8"
+    page_encoding         = "utf-8"
+    mastheadfile          = "mh_bbc.gif"
+    coverfile             = "cv_bbc.jpg"
+    oldest_article        = 1
+    
+    feeds = [
+            (u'BBC Chinese', 'http://www.bbc.com/zhongwen/simp/index.xml'),
+            ]
+    
+    def fetcharticle(self, url, opener, decoder):
+        #每个URL都增加一个后缀full=y，如果有分页则自动获取全部分页
+        url += '?full=y'
+        return BaseFeedBook.fetcharticle(self,url,opener,decoder)
+        
